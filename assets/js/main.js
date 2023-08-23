2c5f396674cdec1185ff29f312353914df9bb3df
Austin Swisher <123541711+swishmeister@users.noreply.github.com>
2023-08-23 17:12:07 -0400

    Add files via upload
    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+var popouts = ["popout-rangers", "popout-poe"]
+function openPopout(id) {
+	hideAllPopouts()
+	var popout = document.getElementById(id)
+	if (popout != undefined) {
+		popout.style.display = "block"
+	}
+	document.getElementsByTagName("body")[0].style.overflow = "hidden"
+}
+function hideAllPopouts() {
+	for (let id of popouts) {
+		var popout = document.getElementById(id)
+		if (popout != undefined) {
+			popout.style.display = "none"
+		}
+	}
+	document.getElementsByTagName("body")[0].style.overflow = "inherit"
+}
